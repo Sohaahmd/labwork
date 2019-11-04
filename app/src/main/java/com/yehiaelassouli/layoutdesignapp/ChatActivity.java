726b6d2ecb53952b147722e98bb160457903743b
Sohaahmd <dreamlover.2009@hotmail.com>
2019-11-04 21:42:53 +0200

    first

---
+++ app/src/main/java/com/yehiaelassouli/layoutdesignapp/ChatActivity.java
@@ -0,0 +1,14 @@
+package com.yehiaelassouli.layoutdesignapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
